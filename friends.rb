91187cbf557519e1fbfce9fc6a44e13eb99c7cac
Steven Stackhouse <stackmm@gmail.com>
2023-02-23 13:59:07 -0700

    Add second best friend variable and conditional

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
 best_friend = "Cameron"
+second_best_friend = "Brennan"
 
 if best_friend == "Cameron"
     puts "Hello Cameron"
+elsif second_best_friend == "Brennan"
+    puts "Hello Brennan"
 else
     puts "Hello other friend"
 end
